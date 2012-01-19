7b2a3197bddfa94505c6d18b4cd1c469088479ee
Santiago Lopez <santiago.lopez.pina@gmail.com>
2012-01-19 22:55:50 +0100

    Versión con bucle de menu

--- Comunicaciones/Python/escribir.py
+++ Comunicaciones/Python/escribir.py
@@ -1,4 +1,7 @@
 import serial
 ser = serial.Serial('/dev/tty.usbserial-A600IP3Q', 9600)
-cadena = raw_input()
-ser.write(cadena)
+cadena = ''
+print('Introduzca una letra, escriba exit para salir')
+while (cadena != "exit"):
+	cadena = raw_input()
+	ser.write(cadena)
